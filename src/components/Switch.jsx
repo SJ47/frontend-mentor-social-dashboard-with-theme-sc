40ad8c96c7b3a7343c88c1b818af3a5eae58cf9e
Scott Johnston <scott@umachan.co.uk>
2021-10-14 20:38:32 +0100

    slider switch position working

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -7,8 +7,8 @@ const StyledSwitch = styled.div`
     .switch {
         position: relative;
         display: inline-block;
-        width: 60px;
-        height: 34px;
+        width: 40px;
+        height: 20px;
     }
 
     /* Hide default HTML checkbox */
@@ -22,10 +22,10 @@ const StyledSwitch = styled.div`
     .slider {
         position: absolute;
         cursor: pointer;
-        top: 0;
+        top: 10px;
         left: 0;
         right: 0;
-        bottom: 0;
+        bottom: -10px;
         background-color: #ccc;
         -webkit-transition: 0.4s;
         transition: 0.4s;
@@ -34,10 +34,10 @@ const StyledSwitch = styled.div`
     .slider:before {
         position: absolute;
         content: "";
-        height: 26px;
-        width: 26px;
-        left: 4px;
-        bottom: 4px;
+        height: 16px;
+        width: 16px;
+        left: 3px;
+        bottom: 2.5px;
         background-color: white;
         -webkit-transition: 0.4s;
         transition: 0.4s;
@@ -52,9 +52,9 @@ const StyledSwitch = styled.div`
     }
 
     input:checked + .slider:before {
-        -webkit-transform: translateX(26px);
-        -ms-transform: translateX(26px);
-        transform: translateX(26px);
+        -webkit-transform: translateX(18px);
+        -ms-transform: translateX(18px);
+        transform: translateX(18px);
     }
 
     /* Rounded sliders */
